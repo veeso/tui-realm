c02f58ce27e17cf277386d53cde3919404f47d1a
veeso <christian.visintin1997@gmail.com>
2021-04-10 20:56:13 +0200

    Fixed license header

--- src/props/builder.rs
+++ src/props/builder.rs
@@ -7,7 +7,7 @@
 /**
  * MIT License
  *
- * termscp - Copyright (c) 2021 Christian Visintin
+ * tui-realm - Copyright (C) 2021 Christian Visintin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
