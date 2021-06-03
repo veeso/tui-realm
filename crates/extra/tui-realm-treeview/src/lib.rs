98b553fd75ea492a479c7cb3b21cc45e7a13d5a5
veeso <christian.visintin1997@gmail.com>
2021-06-03 18:14:09 +0200

    chore: tree/Node data types

--- crates/extra/tui-realm-treeview/src/lib.rs
+++ crates/extra/tui-realm-treeview/src/lib.rs
@@ -46,7 +46,92 @@
 extern crate tui_tree_widget;
 extern crate tuirealm;
 
-use tuirealm::{Component, PropPayload, PropValue, Props};
+use tuirealm::{Component, PropPayload, PropValue, Props, PropsBuilder};
+
+// -- structs
+
+/// ## Tree
+///
+/// represent the tree data structure inside the component
+#[derive(Debug)]
+pub struct Tree {
+    root: Node,
+}
+
+impl Tree {
+    /// ### new
+    ///
+    /// Instantiates a new `Tree`
+    pub fn new(root: Node) -> Self {
+        Self { root }
+    }
+
+    /// ### query
+    ///
+    /// Query tree for a certain node
+    pub fn query(&self, id: &str) -> Option<&Node> {
+        self.root.query(id)
+    }
+}
+
+/// ## Node
+///
+/// Describes a node inside the `Tree`
+#[derive(Debug)]
+pub struct Node {
+    id: String,    // Must uniquely identify the node in the tree
+    label: String, // The text to display associated to the node
+    children: Vec<Node>,
+}
+
+impl Node {
+    /// ### new
+    ///
+    /// Instantiates a new `Node`
+    pub fn new(id: &str, label: &str) -> Self {
+        Self {
+            id: id.to_string(),
+            label: label.to_string(),
+            children: vec![],
+        }
+    }
+
+    /// ### with_children
+    ///
+    /// Sets Node children
+    pub fn with_children(mut self, children: Vec<Node>) -> Self {
+        self.children = children;
+        self
+    }
+
+    /// ### add_child
+    ///
+    /// Create a new child in this Node
+    pub fn add_child(mut self, child: Node) -> Self {
+        self.children.push(child);
+        self
+    }
+
+    /// ### query
+    ///
+    /// Search for `id` inside Node's children (or is itself)
+    pub fn query(&self, id: &str) -> Option<&Self> {
+        if self.id.as_str() == id {
+            Some(&self)
+        } else {
+            // Recurse search
+            self.children
+                .iter()
+                .map(|x| x.query(id))
+                .filter(|x| x.is_some())
+                .flatten()
+                .next()
+        }
+    }
+}
+
+// TODO: to propPayload
+// TODO: from prop payload
 
 // -- states
 
@@ -61,7 +146,47 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_tree() {
+        // -- Build
+        let tree: Tree = Tree::new(
+            Node::new("/", "/")
+                .add_child(
+                    Node::new("/bin", "bin/")
+                        .add_child(Node::new("/bin/ls", "ls"))
+                        .add_child(Node::new("/bin/pwd", "pwd")),
+                )
+                .add_child(
+                    Node::new("/home", "home/").add_child(
+                        Node::new("/home/omar", "omar/")
+                            .add_child(Node::new("/home/omar/readme.md", "readme.md"))
+                            .add_child(Node::new("/home/omar/changelog.md", "changelog.md")),
+                    ),
+                ),
+        );
+        assert_eq!(tree.root.id.as_str(), "/");
+        assert_eq!(tree.root.label.as_str(), "/");
+        assert_eq!(tree.root.children.len(), 2);
+        let bin: &Node = &tree.root.children[0];
+        assert_eq!(bin.id.as_str(), "/bin");
+        assert_eq!(bin.label.as_str(), "bin/");
+        assert_eq!(bin.children.len(), 2);
+        let bin_ids: Vec<&str> = bin.children.iter().map(|x| x.id.as_str()).collect();
+        assert_eq!(bin_ids, vec!["/bin/ls", "/bin/pwd"]);
+        let home: &Node = &tree.root.children[1];
+        assert_eq!(home.id.as_str(), "/home");
+        assert_eq!(home.label.as_str(), "home/");
+        assert_eq!(home.children.len(), 1);
+        let omar_home: &Node = &home.children[0];
+        let omar_home_ids: Vec<&str> = omar_home.children.iter().map(|x| x.id.as_str()).collect();
+        assert_eq!(
+            omar_home_ids,
+            vec!["/home/omar/readme.md", "/home/omar/changelog.md"]
+        );
+        // -- Query
+        assert_eq!(
+            tree.query("/home/omar/changelog.md").unwrap().id.as_str(),
+            "/home/omar/changelog.md"
+        );
+        assert!(tree.query("ommlar").is_none());
     }
 }
