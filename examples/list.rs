d98d824d6bfd47b7619ebf6f2f2262f5e3848bd7
veeso <christian.visintin1997@gmail.com>
2021-07-15 16:53:26 +0200

    Table is now List; merged ScrollTable into Table

--- examples/list.rs
+++ examples/list.rs
@@ -33,7 +33,7 @@ use utils::keymap::*;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use tuirealm::components::{label, table};
+use tuirealm::components::{label, list};
 use tuirealm::props::borders::{BorderType, Borders};
 use tuirealm::props::{TableBuilder, TextSpan, TextSpanBuilder};
 use tuirealm::{Msg, PropsBuilder, Update, View};
@@ -43,6 +43,8 @@ use tui::style::Color;
 
 const COMPONENT_TABLE: &str = "table";
 const COMPONENT_TABLE_2: &str = "table2";
+const COMPONENT_SCROLLTABLE: &str = "scroll_list1";
+const COMPONENT_SCROLLTABLE_2: &str = "scroll_list2";
 const COMPONENT_EVENT: &str = "LABEL";
 
 struct Model {
@@ -88,10 +90,10 @@ fn main() {
     // Mount the component you need; we'll use a Label and an Input
     myview.mount(
         COMPONENT_TABLE,
-        Box::new(table::Table::new(
-            table::TablePropsBuilder::default()
+        Box::new(list::List::new(
+            list::ListPropsBuilder::default()
                 .with_borders(Borders::ALL, BorderType::Thick, Color::Blue)
-                .with_table(
+                .with_rows(
                     Some(String::from("My table data")),
                     TableBuilder::default()
                         .add_col(TextSpan::from("0"))
@@ -196,11 +198,234 @@ fn main() {
     );
     myview.mount(
         COMPONENT_TABLE_2,
-        Box::new(table::Table::new(
-            table::TablePropsBuilder::default()
+        Box::new(list::List::new(
+            list::ListPropsBuilder::default()
                 .with_borders(Borders::ALL, BorderType::Thick, Color::Blue)
-                .with_table(
+                .with_rows(
                     Some(String::from("My table data")),
+                    TableBuilder::default()
+                        .add_col(TextSpan::from("0"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("andreas")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("1"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("bohdan")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("2"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("charlie")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("3"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("denis")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("4"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("ector")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("5"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("frank")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("6"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("giulio")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("7"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("hermes")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("8"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("italo")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("9"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("lamar")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("10"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("mark")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("11"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("napalm")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .build(),
+                )
+                .build(),
+        )),
+    );
+    myview.mount(
+        COMPONENT_SCROLLTABLE,
+        Box::new(list::List::new(
+            list::ListPropsBuilder::default()
+                .with_borders(Borders::ALL, BorderType::Thick, Color::Blue)
+                .with_highlighted_str(Some("🚀"))
+                .with_highlighted_color(Color::LightBlue)
+                .scrollable(true)
+                .with_rows(
+                    Some(String::from("My scrollable data")),
+                    TableBuilder::default()
+                        .add_col(TextSpan::from("0"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("andreas")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("1"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("bohdan")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("2"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("charlie")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("3"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("denis")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("4"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("ector")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("5"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("frank")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("6"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("giulio")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("7"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("hermes")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("8"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("italo")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("9"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("lamar")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("10"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("mark")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .add_row()
+                        .add_col(TextSpan::from("11"))
+                        .add_col(TextSpan::from(" "))
+                        .add_col(
+                            TextSpanBuilder::new("napalm")
+                                .with_foreground(Color::Cyan)
+                                .build(),
+                        )
+                        .build(),
+                )
+                .build(),
+        )),
+    );
+    myview.mount(
+        COMPONENT_SCROLLTABLE_2,
+        Box::new(list::List::new(
+            list::ListPropsBuilder::default()
+                .with_borders(Borders::ALL, BorderType::Thick, Color::Blue)
+                .with_highlighted_str(Some("🚀"))
+                .with_max_scroll_step(4)
+                .scrollable(true)
+                .with_highlighted_color(Color::LightBlue)
+                .with_rows(
+                    Some(String::from("My scrollable data")),
                     TableBuilder::default()
                         .add_col(TextSpan::from("0"))
                         .add_col(TextSpan::from(" "))
@@ -311,7 +536,7 @@ fn main() {
         )),
     );
     // We need to give focus to input then
-    myview.active(COMPONENT_TABLE);
+    myview.active(COMPONENT_SCROLLTABLE);
     // Now we use the Model struct to keep track of some states
     let mut model: Model = Model::new(myview);
     // let's loop until quit is true
@@ -346,6 +571,8 @@ fn view(ctx: &mut Context, view: &View) {
                 [
                     Constraint::Length(10),
                     Constraint::Length(6),
+                    Constraint::Length(10),
+                    Constraint::Length(6),
                     Constraint::Length(1),
                 ]
                 .as_ref(),
@@ -353,7 +580,9 @@ fn view(ctx: &mut Context, view: &View) {
             .split(f.size());
         view.render(COMPONENT_TABLE, f, chunks[0]);
         view.render(COMPONENT_TABLE_2, f, chunks[1]);
-        view.render(COMPONENT_EVENT, f, chunks[2]);
+        view.render(COMPONENT_SCROLLTABLE, f, chunks[2]);
+        view.render(COMPONENT_SCROLLTABLE_2, f, chunks[3]);
+        view.render(COMPONENT_EVENT, f, chunks[4]);
     });
 }
 
@@ -363,12 +592,12 @@ impl Update for Model {
         match ref_msg {
             None => None, // Exit after None
             Some(msg) => match msg {
-                (COMPONENT_TABLE, &MSG_KEY_TAB) => {
-                    self.view.active(COMPONENT_TABLE_2);
+                (COMPONENT_SCROLLTABLE, &MSG_KEY_TAB) => {
+                    self.view.active(COMPONENT_SCROLLTABLE_2);
                     None
                 }
-                (COMPONENT_TABLE_2, &MSG_KEY_TAB) => {
-                    self.view.active(COMPONENT_TABLE);
+                (COMPONENT_SCROLLTABLE_2, &MSG_KEY_TAB) => {
+                    self.view.active(COMPONENT_SCROLLTABLE);
                     None
                 }
                 (_, &MSG_KEY_ESC) => {
