d7d0ebfddd34b0f3b55323114810b074ea8000e7
veeso <christian.visintin1997@gmail.com>
2021-10-12 11:00:02 +0200

    refactor: component to core

--- crates/tuirealm/src/lib.rs
+++ crates/tuirealm/src/lib.rs
@@ -52,9 +52,6 @@ extern crate regex;
 extern crate thiserror;
 extern crate tui as tuirs;
 
-// Ext
-use tuirs::layout::Rect;
-
 // -- modules
 pub mod adapter;
 mod core;
@@ -66,97 +63,6 @@ pub mod utils;
 pub use self::core::command::{self, Cmd, CmdResult};
 pub use self::core::event::{self, Event};
 pub use self::core::props::{self, AttrSelector, Attribute, PropsBuilder};
-pub use self::core::View;
+pub use self::core::{Component, MockComponent, Update, View};
 pub use self::core::{State, Value};
 pub use adapter::{Frame, Terminal};
-
-// -- Component traits
-
-/// ## MockComponent
-///
-/// A Mock Component represents a component which defines all the properties and states it can handle and represent
-/// and the way it should be rendered. It must also define how to behave in case of a `Cmd` (command).
-/// Despite that, it won't define how to behave after an `Event` and it won't send any `Msg`.
-/// The MockComponent is intended to be used as a reusable component to implement your application component.
-///
-/// ### In practice
-///
-/// A real life example would be an Input field.
-/// The mock component is represented by the `Input`, which will define the properties (e.g. max input length, input type, ...)
-/// and by its behaviour (e.g. when the user types 'a', 'a' char is added to input state).
-///
-/// In your application though, you may use a `IpAddressInput` which is the `Component` using the `Input` mock component.
-/// If you want more example, just dive into the `examples/` folder in the project root.
-pub trait MockComponent {
-    /// ### view
-    ///
-    /// Based on the current properties and states, renders the component in the provided area frame.
-    /// Render can also mutate the component state if this is required
-    fn view(&mut self, frame: &mut Frame, area: Rect);
-
-    /// ### query
-    ///
-    /// Query attribute of component properties.
-    fn query(&self, attr: AttrSelector) -> Option<Attribute>;
-
-    /// ### attr
-    ///
-    /// Set attribute to properties.
-    /// `query` describes the name, while `attr` the value it'll take
-    fn attr(&mut self, query: AttrSelector, attr: Attribute);
-
-    /// ### state
-    ///
-    /// Get current state from component
-    fn state(&self) -> State;
-
-    /// ### perform
-    ///
-    /// Perform a command on the component.
-    /// The command will may change the component state.
-    /// The method returns the result of the command applied (what changed if any)
-    fn perform(&mut self, cmd: Cmd) -> CmdResult;
-}
-
-/// ## Component
-///
-/// The component describes the application level component, which is a wrapper around the `MockComponent`,
-/// which, in addition to all the methods exposed by the mock, it will handle the event coming from the `View`.
-/// The Event are passed to the `on` method, which will eventually return a `Msg`,
-/// which is defined in your application as an enum. (Don't forget to derive `PartialEq` for your enum).
-/// In your application you should have a Component for each element on your UI, but the logic to implement
-/// is very tiny, since the most of the work should already be done into the `MockComponent`
-/// and many of them are available in the standard library at <https://github.com/veeso/tui-realm-stdlib>.
-///
-/// Don't forget you can find an example in the `examples/` directory and you can discover many more information
-/// about components in the repository documentation.
-pub trait Component<Msg, UserEvent>: MockComponent
-where
-    Msg: PartialEq,
-    UserEvent: std::fmt::Debug + Eq + PartialEq + Clone + PartialOrd,
-{
-    /// ### on
-    ///
-    /// Handle input event and update internal states.
-    /// Returns a Msg to the view.
-    /// If `None` is returned it means there's no message to return for the provided event.
-    fn on(&mut self, ev: Event<UserEvent>) -> Option<Msg>;
-}
-
-// -- update
-
-/// ## Update
-///
-/// The update trait defines the prototype of the function to be used to handle the events coming from the View.
-pub trait Update<Msg, UserEvent>
-where
-    Msg: PartialEq,
-    UserEvent: std::fmt::Debug + Eq + PartialEq + Clone + PartialOrd,
-{
-    /// ### update
-    ///
-    /// update the current state handling a message from the view.
-    /// This function may return a Message,
-    /// so this function has to be intended to be call recursively if necessary
-    fn update(&mut self, view: &mut View<Msg, UserEvent>, msg: Option<Msg>) -> Option<Msg>;
-}
