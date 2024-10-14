8f65b880514fbf6f0c8d17dcb56084c067bb90c9
veeso <christian.visintin@veeso.dev>
2024-10-14 09:27:48 +0200

    fix: subclause_and_not!

--- src/macros.rs
+++ src/macros.rs
@@ -44,6 +44,58 @@ macro_rules! subclause_and {
     };
 }
 
+/// A macro to generate a chain of [`crate::SubClause::And`] from a list of
+/// Ids with the case  [`crate::SubClause::Not`] containing [`crate::SubClause::IsMounted`] for every id.
+///
+/// Why is this useful?
+/// Well, it happens quite often at least in my application to require a subclause for a "Global Listener" item
+/// to have no "Popup" mounted in the application.
+///
+/// ### example
+///
+/// ```rust
+/// use tuirealm::{SubClause, subclause_and_not};
+///
+/// #[derive(Debug, Eq, PartialEq, Clone, Hash)]
+/// pub enum Id {
+///     InputBar,
+///     InputFoo,
+///     InputOmar,
+/// }
+///
+/// let sub_clause = subclause_and_not!(
+///    Id::InputBar,
+///    Id::InputFoo,
+///    Id::InputOmar
+///    );
+///
+/// assert_eq!(
+///     sub_clause,
+///     SubClause::And(
+///         Box::new(SubClause::Not(Box::new(SubClause::IsMounted(Id::InputBar)))),
+///         Box::new(SubClause::And(
+///             Box::new(SubClause::Not(Box::new(SubClause::IsMounted(Id::InputFoo)))),
+///             Box::new(SubClause::Not(Box::new(SubClause::IsMounted(
+///                 Id::InputOmar
+///             ))))
+///         ))
+///     )
+///  );
+/// ```
+///
+#[macro_export]
+macro_rules! subclause_and_not {
+    ($id:expr) => {
+        SubClause::not(SubClause::IsMounted($id))
+    };
+    ($id:expr, $($rest:expr),+) => {
+        SubClause::and(
+            SubClause::not(SubClause::IsMounted($id)),
+            subclause_and_not!($($rest),+)
+        )
+    };
+}
+
 /// A macro to generate a chain of [`crate::SubClause::Or`] from a list of
 /// Ids with the case [`crate::SubClause::IsMounted`] for every id.
 ///
