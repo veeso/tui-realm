c9d64a4ef47dac50a80ee182a9928af41416612e
hasezoey <hasezoey@gmail.com>
2026-01-20 15:20:34 +0100

    feat(adapter::crossterm): add ability to set custom Terminal options
    To allow Inline viewports

--- crates/tuirealm/src/terminal/adapter/crossterm.rs
+++ crates/tuirealm/src/terminal/adapter/crossterm.rs
@@ -3,10 +3,10 @@ use crossterm::execute;
 use crossterm::terminal::{
     EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
 };
-use ratatui::Terminal;
 
 use super::{TerminalAdapter, TerminalResult};
 use crate::ratatui::backend::CrosstermBackend;
+use crate::ratatui::{Terminal, TerminalOptions};
 use crate::terminal::TerminalError;
 
 /// CrosstermTerminalAdapter is the adapter for the [`crossterm`] terminal
@@ -17,10 +17,16 @@ pub struct CrosstermTerminalAdapter {
 }
 
 impl CrosstermTerminalAdapter {
-    /// Create a new instance of the CrosstermTerminalAdapter
+    /// Create a new crossterm instance with default ratatui Terminal options
     pub fn new() -> TerminalResult<Self> {
+        Self::new_with_options(TerminalOptions::default())
+    }
+
+    /// Create a new crossterm instance with custom ratatui Terminal options
+    pub fn new_with_options(options: TerminalOptions) -> TerminalResult<Self> {
         let backend = CrosstermBackend::new(std::io::stdout());
-        let terminal = Terminal::new(backend).map_err(|_| TerminalError::CannotConnectStdout)?;
+        let terminal = Terminal::with_options(backend, options)
+            .map_err(|_| TerminalError::CannotConnectStdout)?;
 
         Ok(Self { terminal })
     }
