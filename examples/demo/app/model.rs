b75bff2027917c73d14cb13f26f9d5d44cfe5bbd
hasezoey <hasezoey@gmail.com>
2026-02-04 14:34:32 +0100

    fix(examples/demo): add keybindings help text
    And refactor to not recommend using "assert" everywhere

--- examples/demo/app/model.rs
+++ examples/demo/app/model.rs
@@ -2,6 +2,7 @@
 //!
 //! app model
 
+use std::error::Error;
 use std::time::{Duration, SystemTime};
 
 use tuirealm::event::NoUserEvent;
@@ -32,7 +33,7 @@ where
 impl Default for Model<CrosstermTerminalAdapter> {
     fn default() -> Self {
         Self {
-            app: Self::init_app(),
+            app: Self::init_app().expect("Failed to mount components"),
             quit: false,
             redraw: true,
             terminal: Self::init_adapter().expect("Cannot initialize terminal"),
@@ -59,29 +60,31 @@ where
         assert!(
             self.terminal
                 .draw(|f| {
-                    let chunks = Layout::default()
+                    let [help, clock, letter, digit, label] = Layout::default()
                         .direction(Direction::Vertical)
                         .margin(1)
                         .constraints(
                             [
-                                Constraint::Length(3), // Clock
+                                Constraint::Length(1), // Help text
+                                Constraint::Length(2), // Clock
                                 Constraint::Length(3), // Letter Counter
                                 Constraint::Length(3), // Digit Counter
                                 Constraint::Length(1), // Label
                             ]
                             .as_ref(),
                         )
-                        .split(f.area());
-                    self.app.view(&Id::Clock, f, chunks[0]);
-                    self.app.view(&Id::LetterCounter, f, chunks[1]);
-                    self.app.view(&Id::DigitCounter, f, chunks[2]);
-                    self.app.view(&Id::Label, f, chunks[3]);
+                        .areas(f.area());
+                    self.app.view(&Id::Help, f, help);
+                    self.app.view(&Id::Clock, f, clock);
+                    self.app.view(&Id::LetterCounter, f, letter);
+                    self.app.view(&Id::DigitCounter, f, digit);
+                    self.app.view(&Id::Label, f, label);
                 })
                 .is_ok()
         );
     }
 
-    fn init_app() -> Application<Id, Msg, NoUserEvent> {
+    fn init_app() -> Result<Application<Id, Msg, NoUserEvent>, Box<dyn Error>> {
         // Setup application
         // NOTE: NoUserEvent is a shorthand to tell tui-realm we're not going to use any custom user event
         // NOTE: the event listener is configured to use the default crossterm input listener and to raise a Tick event each second
@@ -92,57 +95,57 @@ where
                 .crossterm_input_listener(Duration::from_millis(20), 3)
                 .tick_interval(Duration::from_secs(1)),
         );
-        // Mount components
-        assert!(
-            app.mount(
-                Id::Label,
-                Box::new(
-                    Label::default()
-                        .text("Waiting for a Msg...")
-                        .alignment(Alignment::Left)
-                        .background(Color::Reset)
-                        .foreground(Color::LightYellow)
-                        .modifiers(TextModifiers::BOLD),
-                ),
-                Vec::default(),
-            )
-            .is_ok()
-        );
+        // Mount Help text
+        app.mount(
+            Id::Help,
+            Box::new(
+                Label::default()
+                    .text("Switch counters with <TAB>, exit with <ESC>")
+                    .alignment(Alignment::Left)
+                    .background(Color::Reset)
+                    .foreground(Color::DarkGray),
+            ),
+            Vec::default(),
+        )?;
+        // Mount Message label
+        app.mount(
+            Id::Label,
+            Box::new(
+                Label::default()
+                    .text("Waiting for a Msg...")
+                    .alignment(Alignment::Left)
+                    .background(Color::Reset)
+                    .foreground(Color::LightYellow)
+                    .modifiers(TextModifiers::BOLD),
+            ),
+            Vec::default(),
+        )?;
         // Mount clock, subscribe to tick
-        assert!(
-            app.mount(
-                Id::Clock,
-                Box::new(
-                    Clock::new(SystemTime::now())
-                        .alignment(Alignment::Center)
-                        .background(Color::Reset)
-                        .foreground(Color::Cyan)
-                        .modifiers(TextModifiers::BOLD)
-                ),
-                vec![Sub::new(SubEventClause::Tick, SubClause::Always)]
-            )
-            .is_ok()
-        );
+        app.mount(
+            Id::Clock,
+            Box::new(
+                Clock::new(SystemTime::now())
+                    .alignment(Alignment::Center)
+                    .background(Color::Reset)
+                    .foreground(Color::Cyan)
+                    .modifiers(TextModifiers::BOLD),
+            ),
+            vec![Sub::new(SubEventClause::Tick, SubClause::Always)],
+        )?;
         // Mount counters
-        assert!(
-            app.mount(
-                Id::LetterCounter,
-                Box::new(LetterCounter::new(0)),
-                Vec::new()
-            )
-            .is_ok()
-        );
-        assert!(
-            app.mount(
-                Id::DigitCounter,
-                Box::new(DigitCounter::new(5)),
-                Vec::default()
-            )
-            .is_ok()
-        );
+        app.mount(
+            Id::LetterCounter,
+            Box::new(LetterCounter::new(0)),
+            Vec::new(),
+        )?;
+        app.mount(
+            Id::DigitCounter,
+            Box::new(DigitCounter::new(5)),
+            Vec::default(),
+        )?;
         // Active letter counter
-        assert!(app.active(&Id::LetterCounter).is_ok());
-        app
+        app.active(&Id::LetterCounter)?;
+        Ok(app)
     }
 }
 
