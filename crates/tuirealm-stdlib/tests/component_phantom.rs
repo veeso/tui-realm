4a2c985213977e1b18a9b90077f1e5a137e38bc1
Christian Visintin <christian.visintin@veeso.dev>
2026-03-25 11:50:11 +0100

    test: add shared test helpers and phantom component tests (#191)

---
+++ crates/tuirealm-stdlib/tests/component_phantom.rs
@@ -0,0 +1,18 @@
+mod common;
+
+use tui_realm_stdlib::components::Phantom;
+use tuirealm::component::Component;
+use tuirealm::state::State;
+
+#[test]
+fn test_phantom_state_is_none() {
+    let component = Phantom::default();
+    assert_eq!(component.state(), State::None);
+}
+
+#[test]
+fn test_phantom_snapshot_default() {
+    let mut component = Phantom::default();
+    let rendered = common::render_to_string(&mut component, 40, 5);
+    insta::assert_snapshot!("phantom_default", rendered);
+}
