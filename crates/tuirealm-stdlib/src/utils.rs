1293be2f4366d9db8e9524bf56abf4a62b92e493
hasezoey <hasezoey@gmail.com>
2026-03-29 14:40:11 +0200

    feat(stdlib::utils): add function to wrap ratatui Lines
    Without trimming, First-fit approach.

--- crates/tuirealm-stdlib/src/utils.rs
+++ crates/tuirealm-stdlib/src/utils.rs
@@ -54,8 +54,143 @@ pub fn wrap_spans<'a, 'b: 'a>(spans: &[&'b Span<'a>], width: usize) -> Vec<Line<
     if !line_spans.is_empty() {
         res.push(Line::from(line_spans));
     }
-    // return res
+
     res
+}
+
+/// Make a new empty [`Line`], but with the original style applied.
+#[inline]
+fn make_new_line<'a>(orig: &Line<'a>) -> Line<'a> {
+    Line::default().style(orig.style)
+}
+
+/// Commit the current `newline` and create a new one in its place
+#[inline]
+fn commit_line<'a>(newline: &mut Line<'a>, newlines: &mut Vec<Line<'a>>, orig: &Line<'a>) {
+    let mut final_line = make_new_line(orig);
+    std::mem::swap(newline, &mut final_line);
+    newlines.push(final_line);
+}
+
+/// Wrap a single [`Span`] into multiple [`Line`]s.
+///
+/// Returns the amount of consumed width in the last line.
+fn wrap_single_span<'a>(
+    span: &'a Span<'a>,
+    newlines: &mut Vec<Line<'a>>,
+    newline: &mut Line<'a>,
+    orig_line: &Line<'a>,
+    width: usize,
+    consumed_width: &mut usize,
+) -> usize {
+    let mut remainder_width = width - *consumed_width;
+
+    // textwrap seemingly adds at least *one* character if the given width is 0
+    // so lets commit the line here so that we dont run into that case.
+    if remainder_width == 0 && newline.width() != 0 {
+        commit_line(newline, newlines, orig_line);
+        remainder_width = width;
+    }
+
+    // Use textwrap for the actual splitting.
+    // We know here that wrapping *is* necessary.
+    let words = textwrap::WordSeparator::AsciiSpace.find_words(&span.content);
+    let split_words =
+        textwrap::word_splitters::split_words(words, &textwrap::WordSplitter::HyphenSplitter);
+    let broken_words = textwrap::core::break_words(split_words, remainder_width);
+
+    let line_widths = [remainder_width, width];
+    let wrapped_words = textwrap::WrapAlgorithm::FirstFit.wrap(&broken_words, &line_widths);
+
+    // The index into "span.content" which is already consumed
+    let mut consumed_idx = 0;
+    let last_idx = wrapped_words.len().saturating_sub(1);
+    let mut final_consumed_width = 0;
+    // Each "words" loop represents a final line, except for the last iteration.
+    for (idx, words) in wrapped_words.iter().enumerate() {
+        if words.is_empty() {
+            continue;
+        }
+
+        // The following is disabled as this can (in its current form) only catch some whitespaces to trim
+        // but it would then not fully align with the fast-path in the other function.
+        // so for our purposes, it is not worth it.
+        // // only trim the last whitespace *if* we know we commit the line here
+        // // as otherwise, we dont know if something *might* follow
+        // let minus_whitespace = if idx != last_idx {
+        //     words.last().map_or(0, |word| word.whitespace.len())
+        // } else {
+        //     0
+        // };
+        let minus_whitespace = 0;
+
+        // length in bytes of the current words line
+        let len = words
+            .iter()
+            .map(|word| word.len() + word.whitespace.len())
+            .sum::<usize>()
+            - minus_whitespace;
+
+        let split_text = &span.content[consumed_idx..consumed_idx + len];
+        consumed_idx += len + minus_whitespace;
+
+        let newspan = Span::styled(split_text, span.style);
+        newline.push_span(newspan);
+
+        // unless this is the last loop, there are more lines to come
+        if idx != last_idx {
+            commit_line(newline, newlines, orig_line);
+        } else {
+            final_consumed_width = newline.width();
+        }
+    }
+
+    final_consumed_width
+}
+
+/// Wrap the given lines to fit within `width`.
+pub fn wrap_lines<'a, 'b: 'a>(lines: &[&'b Line<'a>], width: usize) -> Vec<Line<'a>> {
+    // Prepare result (capacity will be at least lines.len)
+    let mut new_lines: Vec<Line> = Vec::with_capacity(lines.len());
+
+    for line in lines {
+        // fast path for when no wrapping is necessary
+        if line.width() <= width {
+            new_lines.push(borrow_clone_line(line));
+            continue;
+        }
+
+        // Width that already has been consumed with the current line iteration
+        let mut consumed_width: usize = 0;
+        let mut newline = make_new_line(line);
+
+        for span in line.iter() {
+            // fast path for when no wrapping is necessary on a span-level
+            let span_width = span.content.width();
+            if span_width <= width - consumed_width {
+                newline.push_span(borrow_clone_span(span));
+                consumed_width += span_width;
+                continue;
+            }
+
+            let new_consumed = wrap_single_span(
+                span,
+                &mut new_lines,
+                &mut newline,
+                line,
+                width,
+                &mut consumed_width,
+            );
+            consumed_width = new_consumed;
+        }
+
+        // commit the final newline, if it is not empty
+        if !newline.spans.is_empty() {
+            new_lines.push(newline);
+        }
+    }
+
+    new_lines
 }
 
 /// Construct a [`Block`] widget from the given properties.
@@ -209,5 +344,114 @@ mod test {
         assert_eq!(calc_utf8_cursor_position(chars.as_slice()), 4);
         let chars: Vec<char> = vec!['我', '之', '😄'];
         assert_eq!(calc_utf8_cursor_position(chars.as_slice()), 6);
+    }
+
+    mod lines_wrap {
+        use pretty_assertions::assert_eq;
+        use tuirealm::props::{LineStatic, SpanStatic, Style};
+        use tuirealm::ratatui::text::Span;
+
+        use crate::utils::wrap_lines;
+
+        #[test]
+        fn should_not_do_any_wrapping() {
+            // empty
+            assert_eq!(
+                wrap_lines(&[&LineStatic::default()], 10),
+                [LineStatic::default()]
+            );
+
+            // single span, fits within width
+            assert_eq!(
+                wrap_lines(&[&LineStatic::raw("test")], 10),
+                [LineStatic::raw("test")]
+            );
+
+            // multi span, fits within width
+            assert_eq!(
+                wrap_lines(
+                    &[&LineStatic::from_iter([
+                        SpanStatic::raw("hello"),
+                        SpanStatic::raw("there")
+                    ])],
+                    10
+                ),
+                [LineStatic::from_iter([
+                    SpanStatic::raw("hello"),
+                    SpanStatic::raw("there")
+                ])]
+            );
+        }
+
+        #[test]
+        fn should_wrap_single_span() {
+            assert_eq!(
+                wrap_lines(&[&LineStatic::raw("something really long")], 10),
+                [
+                    LineStatic::raw("something "),
+                    LineStatic::raw("really "),
+                    LineStatic::raw("long")
+                ]
+            );
+
+            // should preserve styles
+            assert_eq!(
+                wrap_lines(
+                    &[&LineStatic::from(Span::styled(
+                        "something really long",
+                        Style::default().crossed_out()
+                    ))
+                    .style(Style::default().italic())],
+                    10
+                ),
+                [
+                    LineStatic::from(Span::styled("something ", Style::default().crossed_out()))
+                        .style(Style::default().italic()),
+                    LineStatic::from(Span::styled("really ", Style::default().crossed_out()))
+                        .style(Style::default().italic()),
+                    LineStatic::from(Span::styled("long", Style::default().crossed_out()))
+                        .style(Style::default().italic())
+                ]
+            );
+        }
+
+        #[test]
+        fn should_wrap_multi_span() {
+            assert_eq!(
+                wrap_lines(
+                    &[&LineStatic::from_iter([
+                        SpanStatic::raw("something "),
+                        SpanStatic::raw("really "),
+                        SpanStatic::raw("long")
+                    ])],
+                    10
+                ),
+                [
+                    LineStatic::raw("something "),
+                    LineStatic::from_iter([Span::raw("really "), Span::raw("lon")]),
+                    LineStatic::raw("g")
+                ]
+            );
+
+            // should preserve styles
+            assert_eq!(
+                wrap_lines(
+                    &[&LineStatic::from_iter([
+                        SpanStatic::styled("something ", Style::default().crossed_out()),
+                        SpanStatic::raw("really "),
+                        SpanStatic::styled("long", Style::default().italic())
+                    ])],
+                    10
+                ),
+                [
+                    LineStatic::from(Span::styled("something ", Style::default().crossed_out())),
+                    LineStatic::from_iter([
+                        Span::raw("really "),
+                        Span::styled("lon", Style::default().italic())
+                    ]),
+                    LineStatic::from(Span::styled("g", Style::default().italic()))
+                ]
+            );
+        }
     }
 }
